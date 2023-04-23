5764d8309f205298ab0b1581ce5902a252e42d22
Alece Kemp <alece.kemp@gmail.com>
2023-04-22 21:15:29 -0500

    Added two sentences for practice

--- src/About.js
+++ src/About.js
@@ -2,7 +2,13 @@ function About() {
     return (
       <div>
         <h1>About Page</h1>
+        <p>
+          <b>Faithful Connections</b> was designed with intentinal romantic based fellowship in mind. 
+          We developed a balance where you can both meet a lot of people in a short amount of time and
+          maintain the quality time aspect to get to know them. 
+        </p>
       </div>
+      
     );
   }
   
