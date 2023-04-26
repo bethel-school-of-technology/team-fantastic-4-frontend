08b7dc94a731e500103b757cb8a8b6e604385d80
Sara <Sferris42@yahoo.com>
2023-04-25 18:51:27 -0700

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React from "react";
+import React from 'react';
 import Home from './Home';
-import { BrowserRouter, Routes, Route } from "react-routes-dom";
-
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+  return <Home/>
     
-  );
 }
-
 export default App;
