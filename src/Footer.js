2c85c89408baac92b5f94ccadc8764f0cd01bde0
Alece Kemp <alece.kemp@gmail.com>
2023-05-02 22:01:59 -0500

    created Footer

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return(
+        <div className='main-footer'>
+            <div className='container'>
+                <div className='row'>
+                    
+                    <p className='col-sm'>
+                        &copy;{new Date().getFullYear()} Faithful Connections | Owned and designed by Fantastic 4
+                    </p>
+                </div>
+               
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
