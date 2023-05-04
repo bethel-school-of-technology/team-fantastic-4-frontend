f7481f5778bfe20c8647ab5188a234fd1d24a0c4
Jaguarphd <jagbetheltech@gmail.com>
2023-05-03 18:59:05 -0700

    Updated SignUp

--- src/signup.js
+++ src/signup.js
@@ -1,9 +1,48 @@
+import { useState } from 'react';
+
 function Signup() {
-    return (
-      <div>
-        <h1>Sign-Up Page</h1>
-      </div>
-    );
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
   }
-  
-  export default Signup;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+      <input 
+        type="text" 
+        name="username" 
+        value={inputs.username || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <br></br>
+      <label>Enter your age:
+        <input 
+          type="number" 
+          name="age" 
+          value={inputs.age || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <br></br>
+        <input type="submit" />
+    </form>
+  )
+}
+
+export default Signup;
+
+
+
+
+
+
